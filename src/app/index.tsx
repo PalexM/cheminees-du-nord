f5772e0fea3e18d29e4b9e6a3113db6f7abac848
unknown <alexandru.pop@epitech.eu>
2024-06-17 18:58:33 +0200

    push

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@
 export * from "./hero";
 export * from "./layout";
 export * from "./page";
-export * from "./posts";
-export * from "./articles";
+// export * from "./posts";
+// export * from "./articles";
 
 
